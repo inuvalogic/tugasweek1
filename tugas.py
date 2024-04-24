7e282b9f85f6d8e762dee07d7d7ff2d297dadb03
Wisnu Hafid <wisnuhafid@gmail.com>
2024-04-24 20:55:11 +0700

    tugas ke 1

---
+++ tugas.py
@@ -0,0 +1,5 @@
+doing_python_right_now = True
+
+assert doing_python_right_now == True, "If you see a NameError, it means that the variable is not created and assigned a value. An 'Assertion Error' means that the value of the variable is incorrect." 
+print("Exercise 0 is correct") # This line will print if your solution passes the assertion above.
+
